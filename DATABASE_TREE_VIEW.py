b14b251106cd6e55f7c523bb8abd74f81a01bf3a
Arthur Crivilin Del Caro <acrivilindelcaro@gmail.com>
2021-04-13 11:06:17 -0300

    .

--- DATABASE_TREE_VIEW.py
+++ DATABASE_TREE_VIEW.py
@@ -9,8 +9,6 @@ ROOT_DIR = os.path.split(os.path.abspath(__file__))[0]
 Alib.VARIABLES_INIT()
 DB_CLIENT = Alib.CONNECT_DB()
 
-#COMENTARIO ARTHUR ARTHURHTUSBGOASDUJBFGOASG
-
 def fazer_str_porcentagem(superior,inferior):
     try:
         return f"""{Alib.format_numero(superior)}/{Alib.format_numero(inferior)} = {Alib.format_numero(superior*100/inferior)}%"""
